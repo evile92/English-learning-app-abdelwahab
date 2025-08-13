3b00ba9af33f8f76380270b09b686999956a2321
evile92 <abdelwahab.kahoch@gmail.com>
2025-08-13 02:17:43 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: 'class', // <-- هذا السطر هو الإصلاح الأهم
   theme: {
     extend: {},
   },
   plugins: [
-     require('@tailwindcss/typography'), // هذا السطر مهم لتحسين شكل النصوص
+    require('@tailwindcss/typography'),
   ],
 }
