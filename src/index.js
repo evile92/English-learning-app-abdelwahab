b7614f08b3c868653d19d05a10e7dd050be65212
evile92 <abdelwahab.kahoch@gmail.com>
2025-08-19 20:02:30 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { AppProvider } from './context/AppContext'; // <-- (التغيير الأول) استيراد الـ Provider
+import { AppProvider } from './context/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider> {/* <-- (التغيير الثاني) تغليف التطبيق بالكامل */}
+    <AppProvider>
       <App />
     </AppProvider>
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
