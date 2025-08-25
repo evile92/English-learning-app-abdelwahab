d3609ce572e3aa60db88f6f5fb547377b773aaea
evile92 <abdelwahab.kahoch@gmail.com>
2025-08-25 17:48:01 +0000

    Create freestyleSentences.js

---
+++ src/data/freestyleSentences.js
@@ -0,0 +1,14 @@
+// src/data/freestyleSentences.js
+
+export const freestyleSentences = [
+    "The quick brown fox jumps over the lazy dog.",
+    "She sells seashells by the seashore.",
+    "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+    "Peter Piper picked a peck of pickled peppers.",
+    "I have a meeting at three o'clock this afternoon.",
+    "Can you please tell me where the nearest station is?",
+    "The weather is beautiful today, isn't it?",
+    "I would like to order a cup of coffee with a little milk.",
+    "Learning a new language can be challenging but rewarding.",
+    "Technology has changed the way we live and work."
+];
