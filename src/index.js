06863de5c7439929842116482e8e82e32159da60
evile92 <abdelwahab.kahoch@gmail.com>
2025-09-02 04:11:21 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { AppProvider } from './context/AppContext';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+// 1. إنشاء "الجذر" (root) للتطبيق بالطريقة الحديثة
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 2. تصيير التطبيق داخل الجذر
 root.render(
   <React.StrictMode>
     <AppProvider>
@@ -16,4 +17,6 @@ root.render(
   </React.StrictMode>
 );
 
+// 3. تسجيل الـ Service Worker (مهم لتطبيقات الويب التقدمية)
+// يفضل استخدام register بدلاً من unregister للاستفادة من الميزات
 serviceWorkerRegistration.register();
