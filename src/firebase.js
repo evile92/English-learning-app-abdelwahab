80539e7a320f69136b291a29bd2da4ae56d6a4bd
evile92 <abdelwahab.kahoch@gmail.com>
2025-09-15 00:05:51 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // <-- 1. استيراد أداة Firestore
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlU8_VKdSeGLwFt5XpAhu04hfKGqGYg9c",
-  authDomain: "stellar-speak.firebaseapp.com",
-  projectId: "stellar-speak",
-  storageBucket: "stellar-speak.appspot.com",
-  messagingSenderId: "148792919641",
-  appId: "1:148792919641:web:b7d8c6add977cf83bd5348"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -19,7 +20,5 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 
-// --- (بداية الإضافة) ---
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
-// --- (نهاية الإضافة) ---
