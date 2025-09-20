8595f9abbfae0686410038d81df655c442dd067f
evile92 <abdelwahab.kahoch@gmail.com>
2025-09-20 02:24:55 +0000

    Update StellarSpeakLogo.js

--- src/components/StellarSpeakLogo.js
+++ src/components/StellarSpeakLogo.js
@@ -1,16 +1,14 @@
+// src/components/StellarSpeakLogo.js
+
 import React from 'react';
 
 const StellarSpeakLogo = () => (
-    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-            <linearGradient id="logoGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                <stop offset="0%" style={{ stopColor: '#38bdf8', stopOpacity: 1 }} />
-                <stop offset="100%" style={{ stopColor: '#3b82f6', stopOpacity: 1 }} />
-            </linearGradient>
-        </defs>
-        <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27z" fill="url(#logoGradient)" />
-        <circle cx="12" cy="12" r="3.5" fill="white" />
-    </svg>
+    <img 
+        src="/logo.jpg" 
+        alt="Stellar Speak Logo" 
+        width="40" 
+        height="40" 
+    />
 );
 
 export default StellarSpeakLogo;
