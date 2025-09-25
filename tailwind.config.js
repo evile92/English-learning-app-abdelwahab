b9d9e10aa3650936309556658e55ceaba5d92038
evile92 <abdelwahab.kahoch@gmail.com>
2025-09-25 23:55:34 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      // --- ✅ بداية التعديل المطلوب ---
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            'textarea, input': {
-              all: 'unset',
-            },
-          },
-        },
-      }),
-      // --- 🛑 نهاية التعديل المطلوب ---
-    },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
