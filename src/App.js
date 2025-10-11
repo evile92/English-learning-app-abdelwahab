8fb6f4f865e9f0fc3ffa37e5c0041911cb9af5e3
evile92 <abdelwahab.kahoch@gmail.com>
2025-10-11 01:27:59 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -136,9 +136,15 @@ export default function App() {
   };
 
   useEffect(() => {
+    // تشغيل الانميشن بعد تحميل كامل
+    setTimeout(() => {
+        document.body.classList.add('animations-ready');
+    }, 100);
+    
     PWANotificationService.requestPermission();
     PWANotificationService.scheduleStudyReminder();
-  }, []);
+}, []);
+
 
   // إصلاح تسرب الذاكرة: إزالة dependencies لمنع إعادة إنشاء timer
   useEffect(() => {
